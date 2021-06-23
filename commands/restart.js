b87c1ee1e7ad7b6fe15dc47e157ea0baedff1ea2
Brice Coley <yoshiboi18303.t@gmail.com>
2021-06-23 10:29:03 -0400

    Update restart.js

--- commands/restart.js
+++ commands/restart.js
@@ -11,7 +11,7 @@ if(message.author.id === config.ids.ownerID) {
 
   message.reply(`Restarting... <a:windows_loading:854438904325472287>`).then(msg => msg.delete({timeout: "5000"}));
    client.destroy();
-   client.login('ODQ4NzI5Mjc4MDY5MjExMTM2.YLQ2vA.9y4QF4zX2-SaiRwbPKcz2ClFvdE');
+   client.login('No');
   message.reply(`${emojis.animated_emojis.doneEmoji} Finished!`)
   } else {
      message.reply(`${emojis.animated_emojis.crossemoji} You are **NOT** the owner of <@${client.user.id}>!`)
