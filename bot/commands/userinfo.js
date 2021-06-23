e87eabae1349bf46f643c8b71141365cff47ecdc
Brice Coley <yoshiboi18303.t@gmail.com>
2021-06-23 17:57:25 -0400

    Update userinfo.js

--- bot/commands/userinfo.js
+++ bot/commands/userinfo.js
@@ -11,6 +11,7 @@ module.exports = {
     const member = guild.members.cache.get(user.id);
     const mentionedMember = message.guild.member(user);
     const checkStatus = await statusCheck();
+    const usersStatus = await status();
 
     function statusCheck() {
     if(mentionedMember.presence.status === 'idle') {
@@ -19,6 +20,20 @@ module.exports = {
       return "False"
     }
   };
+  
+    const userStatus = mentionedMember.presence.status;
+
+    function status() {
+      if(userStatus === 'online') {
+        return "Online"
+      } else if(userStatus === 'idle') {
+        return "Idle/AFK"
+      } else if(userStatus === 'dnd') {
+        return "Do Not Disturb"
+      } else if(userStatus === 'invisible') {
+        return "Invisible/Offline"
+      }
+    };
 
     const userinfoEmbed = new MessageEmbed()
       .setAuthor(`User info for ${user.username} in ${guild.name}`, user.displayAvatarURL())
@@ -26,38 +41,52 @@ module.exports = {
         {
           name: 'User Tag',
           value: user.tag,
+          inline: true
+        },
+        {
+          name: 'Status',
+          value: usersStatus,
+          inline: true
         },
         {
           name: 'Bot User?',
           value: user.bot,
+          inline: true
         },
         {
           name: 'Nickname',
           value: member.nickname || 'None',
+          inline: true
         },
         {
           name: 'Joined Server On',
           value: new Date(member.joinedTimestamp).toLocaleDateString(),
+          inline: true
         },
         {
           name: 'Joined Discord',
           value: new Date(user.createdTimestamp).toLocaleDateString(),
+          inline: true
         },
         {
           name: 'Role Count (excluding @ everyone role)',
           value: member.roles.cache.size - 1,
+          inline: true
         },
         {
           name: 'Last Message Sent',
-          value: member.lastMessage - message
+          value: member.lastMessage - message,
+          inline: true
         },
         {
           name: "Presence",
-          value: mentionedMember.user.presence.activity || "None!"
+          value: mentionedMember.user.presence.activity || "None!",
+          inline: true
         },
         {
           name: "AFK?",
-          value: checkStatus
+          value: checkStatus,
+          inline: true
         }
       );
 
