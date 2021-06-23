271243d12716e3516a46dc774d7c09a7ba56d7b6
Brice Coley <yoshiboi18303.t@gmail.com>
2021-06-23 12:03:20 -0400

    Moving Commands Directory

--- commands/test.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: "test",
-    aliases: [],
-    description: "hgnfbf",
-    execute(client, message) {
-        message.channel.send("I'm testing something with this command.")
-    }
-}
